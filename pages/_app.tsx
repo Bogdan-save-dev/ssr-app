8d4b4c4f3190084ebd09f98560b90b7346c88bc6
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-15 16:09:59 +0300

    Htag component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import { AppProps } from '../node_modules/next/app'
+import Head from '../node_modules/next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Top App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
