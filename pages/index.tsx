3cf597ba936efbc52666f10289611983be9dc78c
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-15 17:18:07 +0300

    BTN component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import { Htag } from '../components'
+import { Htag, Button } from '../components'
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h2">Text</Htag>
+      <Button appearance="ghost">Button</Button>
     </div>
   )
 }
