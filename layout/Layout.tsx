ab49a9f23fa0376b1c23a72acfef5e3bf9f504d0
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-22 10:51:13 +0300

    function wrapper for Layout is added

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -4,8 +4,9 @@ import { LayoutProps } from './Layout.props'
 import { Sidebar } from './Sidebar/Sidebar'
 
 import styles from './Layout.module.css'
+import { FunctionComponent } from 'react'
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <>
       <Header />
@@ -16,4 +17,16 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
       <Footer />
     </>
   )
+}
+
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>,
+) => {
+  return function WithLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    )
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { Htag, Button, Tag, Ptag, Rating } from '../components'
 import { useState } from 'react'
-import { Layout } from '../layout/Layout'
+import { withLayout } from '../layout/Layout'
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4)
 
   return (
-    <Layout>
+    <>
       <Htag tag="h2">Text</Htag>
       <Button appearance="ghost">Button</Button>
       <Button appearance="primary" arrow="down">
@@ -20,6 +20,8 @@ export default function Home(): JSX.Element {
       </Ptag>
       <Tag color="green">Tag</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
-    </Layout>
+    </>
   )
 }
+
+export default withLayout(Home)
