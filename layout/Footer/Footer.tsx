c0836997f885c93e60a8ca1d780b8510c35042d2
bogdan-save-dev <tradeexample99@gmail.com>
2022-08-22 12:21:03 +0300

    grid layout + footer styling

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,14 @@
 import styles from './Footer.module.css'
 import { FooterProps } from './Footer.props'
+import cn from 'classnames'
+import { format } from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <p> {format(new Date(), 'yyyy')} - all rights reserved</p>
+      <a href="#">User Agreement</a>
+      <a href="#">Privacy policy</a>
+    </footer>
+  )
 }
